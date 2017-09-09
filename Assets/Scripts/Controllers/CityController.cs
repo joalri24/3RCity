a2b8ab79aed8fd94db18f09e7b83a76d6b434c98
Jose Alejandro Riaño <joalri24@hotmail.com>
2017-09-09 17:52:55 -0500

    Houses are added to the controller's list when the scene is generated.

--- Assets/Scripts/Controllers/CityController.cs
+++ Assets/Scripts/Controllers/CityController.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CityController : MonoBehaviour {
+/// <summary>
+/// Scene controllers.
+/// It knows the city' status and what building currently exists on it.
+/// It also makes the city's date advance.
+/// </summary>
+public class CityController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
+    // -----------------------------------------------------------
+    // Attributes
+    // -----------------------------------------------------------
+
+    /// <summary>
+    /// Property to access a list with all the houses of the city.
+    /// </summary>
+    public List<House> houses;
+
+    // -----------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------
+
+    // Use this for initialization
+    void Start ()
+    {
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(Input.GetKeyDown("b"))
+        {
+            Debug.Log(houses.Count);
+        }
 	}
 }
