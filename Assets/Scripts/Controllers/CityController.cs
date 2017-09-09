e61c7184fd8d2b9d40a3a88faf2e50027f159ff5
Jose Alejandro Riaño <joalri24@hotmail.com>
2017-09-09 18:52:45 -0500

    Controller advances the day each x seonds,

--- Assets/Scripts/Controllers/CityController.cs
+++ Assets/Scripts/Controllers/CityController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -11,30 +12,131 @@ public class CityController : MonoBehaviour
 {
 
     // -----------------------------------------------------------
-    // Attributes
+    // Attributes and properties
     // -----------------------------------------------------------
 
     /// <summary>
-    /// Property to access a list with all the houses of the city.
+    /// The duration in game days of the current match.
     /// </summary>
+    [Header("Match settings")]
+    [Tooltip("The duration in game days of the current match")]
+    [Range (2,1500)]
+    public int matchLength;
+
+    /// <summary>
+    /// The duration in real life seconds of a day in the game.
+    /// </summary>
+    [Tooltip("The duration in real life seconds of a day in the game")]
+    [Range(0.5f, 200)]
+    public float dayDuration;
+
+    /// <summary>
+    /// The current day on the game.
+    /// </summary>
+    private int currentDay;
+
+    /// <summary>
+    /// The start date on the game.
+    /// </summary>
+    private DateTime startDate;
+
+    /// <summary>
+    /// List with all the houses of the city.
+    /// </summary>   
+    [Header("City buildings")]
+    [Tooltip("List with all the houses of the city")]
     public List<House> houses;
+
+    // TODO: list of other buildings
+
+
+    /// <summary>
+    /// Timer to know when to advance to the next day.
+    /// </summary>
+    private float timer;
+
+    /// <summary>
+    /// Flag to know if the game is paused or not.
+    /// </summary>
+    private bool paused;
+
+    /// <summary>
+    /// Property used to pause and unpause the game.
+    /// </summary>
+    public bool Paused
+    {
+        get {return  paused; }
+        set
+        {
+            paused = value;
+            if (paused)
+            {
+                // TODO:
+                Debug.Log("Paused");
+            }
+            else
+            {
+                Debug.Log("Unpaused");
+            }
+            
+        }
+    }
 
     // -----------------------------------------------------------
     // Methods
     // -----------------------------------------------------------
 
-    // Use this for initialization
-    void Start ()
+    /// <summary>
+    /// Executed the first frame this object is alive.
+    /// </summary>
+    private void Start()
     {
-        
-	}
-	
+        currentDay = 0;
+        timer = 0f;
+        startDate = DateTime.Now;
+        Debug.Log("Current date: " + startDate.AddDays(currentDay).ToString("dd/MM/yyyy"));
+    }
+
+    /// <summary>
+    /// Called to move forward a day on the time.
+    /// </summary>
+    private void AdvanceDay()
+    {
+        currentDay++;
+
+        // TODO: uncomment when the House script is finished.
+        /*foreach (var house in houses)
+        {
+            // house.GenerateGarbage();
+        }*/
+
+        Debug.Log("Current date: " + startDate.AddDays(currentDay).ToString("dd/MM/yyyy"));
+
+        if(currentDay >= matchLength)
+        {
+            // TODO: llamar al método de terminar partida
+            Paused = true;
+            Debug.Log("Time's up!");
+        }
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
-		if(Input.GetKeyDown("b"))
+        // Pause the game when the key is pressed.
+        if (Input.GetKeyDown("pause"))
+            Paused = (Paused) ? false : true;
+        
+        // Add the passed time to the timer if the game is unpaused.
+        if(!Paused)
+            timer += Time.deltaTime;
+
+        // Advance day when the proper amount of time has passed.
+        if (timer >= dayDuration)
         {
-            Debug.Log(houses.Count);
+            AdvanceDay();
+            timer = 0;
         }
+            
 	}
 }
