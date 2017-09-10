3cf8d5cbfc757cfc07dce7c4e54ff622dfb00b49
Juan Pablo Camacho <jp.camacho10@uniandes.edu.co>
2017-09-10 13:25:02 -0500

    clase house y clase garbage sin integrar a unity

--- Assets/Scripts/Buildings/House.cs
+++ Assets/Scripts/Buildings/House.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class House : MonoBehaviour {
+    public Garbage garbage;
+    //Trash can's maximum capacity
+    public int trashCanCapacity;
+    //Trash can's actual garbage 
+    public int trashCanGarbage;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        garbage = new Garbage(0, 0, 0, 0);
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        trashCanGarbage = garbage.regular + garbage.paper + garbage.glass + garbage.metal;
+
+    }
+
+    public void garbageProduction()
+    {
+        System.Random rnd = new System.Random();
+        garbage.regular += rnd.Next(1, 13);  // regular garbage: >= 1 and < 13
+        garbage.paper += rnd.Next(1, 13);    // paper garbage: >= 1 and < 13
+        garbage.glass += rnd.Next(1,13);      // glass garbage: >= 1 and < 13
+        garbage.metal += rnd.Next(1, 13);      // metal garbage: >= 1 and < 13
+
+
+    }
+    
 }
--- Assets/Scripts/Classes/Garbage.cs
+++ Assets/Scripts/Classes/Garbage.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Garbage : MonoBehaviour {
 
+    public int regular;
+    public int paper;
+    public int glass;
+    public int metal;
+
 	// Use this for initialization
 	void Start () {
+        //regular = 0;
+        //paper = 0;
+        //glass = 0;
+        //metal = 0;
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public Garbage(int nRegular, int nPaper, int nGlass, int nMetal)
+    {
+        regular = nRegular;
+        paper = nPaper;
+        glass = nGlass;
+        metal = nMetal;
+    }
+    
 }
